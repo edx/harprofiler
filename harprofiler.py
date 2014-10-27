5ac932195cd897c59efaa3e760e8cc737d0200b9
Corey Goldberg <cgoldberg@gmail.com>
2014-10-27 10:24:52 -0400

    added license to source header

--- harprofiler.py
+++ harprofiler.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
-# Corey Goldberg
-# web page profiler, har exporter
-
+#
+# Corey Goldberg, 2014
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 
 import argparse
 from json import dump
