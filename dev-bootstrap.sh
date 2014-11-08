69aaed23b48fd50c81e6883173974ed2091d9eab
Corey Goldberg <cgoldberg@gmail.com>
2014-11-08 17:47:00 -0500

    updated bootstrap script for dev envs

--- dev-bootstrap.sh
+++ dev-bootstrap.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 # bootstrap local development environment (debian/ubuntu)
 
-sudo apt-get install -y default-jre firefox git python-virtualenv xvfb
-wget https://s3-us-west-1.amazonaws.com/lightbody-bmp/browsermob-proxy-2.0-beta-9-bin.zip -O bmp.zip
-unzip -o bmp.zip
-rm bmp.zip
+SYSTEM_PACKAGES=(default-jre firefox git python-virtualenv unzip xvfb)
+BROWSERMOBPROXY_DIR=browsermob-proxy-2.0-beta-9
+BROWSERMOBPROXY_DOWNLOAD_URL=https://s3-us-west-1.amazonaws.com/lightbody-bmp/$BROWSERMOBPROXY_DIR-bin.zip
+
+
+# Check each system package requirement and install if needed
+for package in ${SYSTEM_PACKAGES[@]}; do
+    dpkg -s $package 2>/dev/null >/dev/null || sudo apt-get -y install $package
+done
+
+# Download and unzip browsermob-proxy server if it doesn't exist
+if [ ! -d "$BROWSERMOBPROXY_DIR" ]; then
+    wget $BROWSERMOBPROXY_DOWNLOAD_URL -O $BROWSERMOBPROXY_DIR-bin.zip
+    unzip $BROWSERMOBPROXY_DIR-bin.zip
+    rm $BROWSERMOBPROXY_DIR-bin.zip
+fi
+
+# Create virtualenv and install requirements
 virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
