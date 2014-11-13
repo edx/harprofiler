7dd5e2483d15a679ec696c894e3a60ac4c49401f
Christine Lytwynec <clytwynec@edx.org>
2014-11-13 16:52:01 -0500

    add travis.yml

--- dev-bootstrap.sh
+++ dev-bootstrap.sh
@@ -2,10 +2,6 @@
 # bootstrap local development environment (debian/ubuntu)
 
 SYSTEM_PACKAGES=(default-jre firefox git python-virtualenv unzip xvfb)
-BROWSERMOBPROXY_DIR=browsermob-proxy-2.0-beta-9
-BROWSERMOBPROXY_ZIP=$BROWSERMOBPROXY_DIR-bin.zip
-BROWSERMOBPROXY_DOWNLOAD_URL=https://s3-us-west-1.amazonaws.com/lightbody-bmp/$BROWSERMOBPROXY_ZIP
-
 
 # Check each system package requirement and install if needed
 sudo apt-get -qq -y update
@@ -13,12 +9,7 @@ for package in ${SYSTEM_PACKAGES[@]}; do
     dpkg -s $package 2>/dev/null >/dev/null || sudo apt-get -y install $package
 done
 
-# Download and unzip browsermob-proxy server if it doesn't exist
-if [ ! -d "$BROWSERMOBPROXY_DIR" ]; then
-    wget $BROWSERMOBPROXY_DOWNLOAD_URL -O $BROWSERMOBPROXY_ZIP
-    unzip $BROWSERMOBPROXY_ZIP
-    rm $BROWSERMOBPROXY_ZIP
-fi
+bash install-bmp.sh
 
 # Create virtualenv and install requirements
 virtualenv env
